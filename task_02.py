339163a85928f366f49dba01a1c62bbbe7e4f8fb
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2024-05-11 11:09:50 +0200

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -1,27 +1,27 @@
 def binary_search(arr, target):
     iterations = 0
     left = 0
-    right = len(arr) -1
+    right = len(arr) - 1
     upper_bound = None
     
     while left <= right:
-        mid = (left + right) //2
+        mid = (left + right) // 2
         iterations += 1
         
-        if arr[mid] < target:
-            left = mid + 1
-        elif arr[mid] > target:
-            left = mid - 1
-            upper_bound = arr[mid] if upper_bound is None or arr[mid] < upper_bound else upper_bound
-        else:
+        if arr[mid] == target:
             return iterations, arr[mid]
-        
-        # If target is not found, return the upper bound
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            upper_bound = arr[mid]
+            right = mid - 1
+    
+    # If target is not found
     return iterations, upper_bound
-        
+
 # Examples:
 sorted_array = [0.1, 0.5, 1.2, 2.3, 3.5, 5.8, 8.9]
-target_value = 2.0
+target_value = 2.3
 iterations, upper_bound = binary_search(sorted_array, target_value)
 print("Кількість ітерацій:", iterations)
-print("Верхня межа", upper_bound)
+print("Верхня межа:", upper_bound)
